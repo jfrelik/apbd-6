05f41492e796494fed57003125a0f5bbf3b17428
Jakub Frelik <git@jfrelik.dev>
2024-04-23 21:11:03 +0200

    chore(project): add project files

---
+++ Warehouse/Warehouse/Program.cs
@@ -0,0 +1,24 @@
+using Warehouse.Warehouse;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IWarehouseService, WarehouseService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.RegisterEndpointsForWarehouse();
+
+app.UseHttpsRedirection();
+app.Run();
